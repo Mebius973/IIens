78c6e3007d329b42b02b134726576829cb5533ac
Mebius <viddafroygeo@hotmail.com>
2015-04-22 20:55:38 +0200

    Fix #1: create News Model, may eventually need to use a string type for publicationDate though

---
+++ IIens/Model/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IIens.Model
+{
+    class News
+    {
+        public String title { get; set; }
+        public String description { get; set; }
+        public String author { get; set; }
+        public DateTime publicationDate { get; set; }
+    }
+}
