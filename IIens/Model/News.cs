3ef67e15ffd7d1ad74febf7aa412cd3b91d695ae
Mebius <viddafroygeo@hotmail.com>
2015-04-23 20:14:28 +0200

    enhanced news model to fit the mvvm pattern

--- IIens/Model/News.cs
+++ IIens/Model/News.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace IIens.Model
 {
-    class News
+    class News : INotifyPropertyChanged
     {
         public String title { get; set; }
         public String description { get; set; }
